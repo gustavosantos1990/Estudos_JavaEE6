6545ca7165031a91e186c84842b6145a306486ca
gustavosantos1990 <>
2017-08-22 00:50:51 -0300

    Commit inicial

---
+++ src/main/java/com/apress/javaee6/chapter02/Main.java
@@ -0,0 +1,35 @@
+
+package com.apress.javaee6.chapter02;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //Cria uma instãncia de livro
+        Book book = new Book();
+        book.setTitle("The Hitchhiker's Guide to the Galaxy");
+        book.setPrice(12.5F);
+        book.setDescription("Science ficton comedy book");
+        book.setIsbn("1-84023-742-2");
+        book.setNbOfPage(354);
+        book.setIllustrations(false);
+        
+        //Obtêm um gerenciador de entidades e uma transação
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("chapter02PU");
+        EntityManager em = emf.createEntityManager();
+        
+        //Persiste o livro na base de dados
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(book);
+        tx.commit();
+        
+        em.close();
+        emf.close();
+    }
+}
